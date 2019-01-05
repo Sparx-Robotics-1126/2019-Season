5a68a028a3f1a540147e0f3cfce3154aa6665dcd
SparX <sparx@gosparx.org>
2019-01-05 15:59:32 -0500

    did even more stuff'

--- src/main/java/frc1126/robot/Main.java
+++ src/main/java/frc1126/robot/Main.java
@@ -5,7 +5,7 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot;
+package frc1126.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
 
