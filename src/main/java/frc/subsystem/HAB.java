6006e8421441917e807bd3c4f1f13af1f5cf1b4e
nick <nick972898@outlook.com>
2019-02-02 12:35:53 -0500

    added states

--- src/main/java/frc/subsystem/HAB.java
+++ src/main/java/frc/subsystem/HAB.java
@@ -11,6 +11,7 @@ package frc.subsystem;
 import frc.robot.IO;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 /**
  * Add your docs here.
@@ -21,7 +22,8 @@ public class HAB extends GenericSubsystem{
 
     private WPI_TalonSRX leadScrewMtr;
     private Encoder leadScrewEncRaw;
-    private boolean isDone = false;
+    private LeadScrewState state;
+    private DigitalInput bottomSensor;
 
     public HAB(){
         super("Hab");
@@ -33,14 +35,43 @@ public class HAB extends GenericSubsystem{
         leadScrewEncRaw = new Encoder(23, 22);
         leadScrewEncRaw.setDistancePerPulse(0.03103);
         leadScrewEncRaw.reset();
+        bottomSensor = new DigitalInput(14);
+        state = LeadScrewState.HOME;
     }
    
+    public enum LeadScrewState{
+        STANDBY,
+        UP,
+        DOWN,
+        HOME;
+    }
+
     @Override
     public void execute(){
-        if(isDone){
-            leadScrewUp();
-        }else{
-            leadScrewDown();
+        switch(state){
+            case STANDBY:
+                break;
+            case UP:
+                if(leadScrewEncRaw.getDistance() > 0){
+                    leadScrewMtr.set(0.3);
+                }else{
+                    leadScrewMtr.set(0.0);
+                }
+                break;
+            case DOWN:
+                if(leadScrewEncRaw.getDistance()< 24){ //gav
+                    leadScrewMtr.set(-0.3); //gav
+                }else{
+                    leadScrewMtr.set(0.0);
+                }
+                break;
+            case HOME:
+                if(!bottomSensor.get()){
+                    leadScrewMtr.set(0.3); 
+                }else{
+                    leadScrewMtr.set(0.0);
+                }
+                break;
         }
         System.out.println("HAB Encoder Value:" + leadScrewEncRaw.getDistance());
     }
@@ -58,23 +89,6 @@ public class HAB extends GenericSubsystem{
     @Override
     public long sleepTime(){
         return 20;
-    }
-
-    public void leadScrewDown(){
-        if(leadScrewEncRaw.getDistance()< 24){ //gav
-            leadScrewMtr.set(-0.3); //gav
-        }else{
-            leadScrewMtr.set(0.0);
-            isDone = true;
-        }
-    }
-
-    public void leadScrewUp(){
-        if(leadScrewEncRaw.getDistance() > 0){
-            leadScrewMtr.set(0.3);
-        }else{
-            leadScrewMtr.set(0.0);
-        }
     }
 
 }
