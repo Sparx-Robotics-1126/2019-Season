7873dc1a50212405fac56dda6348e033824a8df2
DefinitiveAbove <definitiveabove@gmail.com>
2019-02-18 20:52:22 -0500

    haHAA

--- src/main/java/frc/controls/TeleOP.java
+++ src/main/java/frc/controls/TeleOP.java
@@ -84,9 +84,9 @@ public class TeleOP implements Controls {
 		auto.addStep(AutoMethod.DRIVES_WAIT);
 		auto.addStep(AutoMethod.HAB_DOWN);
 		auto.addStep(AutoMethod.HAB_WHEELS_FORWARD, 0.4);
-		auto.addStep(AutoMethod.DRIVES_FORWARD, 0.2, 60);
+//		auto.addStep(AutoMethod.DRIVES_FORWARD, 0.2, 60);
 		auto.addStep(AutoMethod.HAB_WAIT);	
-		auto.addStep(AutoMethod.DRIVES_STOP);
+//		auto.addStep(AutoMethod.DRIVES_STOP);
 		auto.addStep(AutoMethod.HAB_WHEELS_FORWARD, 1);
 		auto.addStep(AutoMethod.AUTO_DELAY, 2.75); 
 		auto.addStep(AutoMethod.HAB_UP);
